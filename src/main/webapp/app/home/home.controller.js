f8b878cdb76b3dcab3ad11d7aa022796bbf2e0ab
jzheadley <zephyrinkg@gmail.com>
2016-11-13 07:51:58 -0500

    Lots of just making things how we want them

--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -1,33 +1,63 @@
-(function() {
+(function () {
     'use strict';
 
     angular
         .module('noteShareApp')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state'];
+    HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state', 'Note', 'NoteSearch', 'CourseSearch'];
 
-    function HomeController ($scope, Principal, LoginService, $state) {
+    function HomeController($scope, Principal, LoginService, $state, Note, NoteSearch, CourseSearch) {
         var vm = this;
 
         vm.account = null;
         vm.isAuthenticated = null;
         vm.login = LoginService.open;
         vm.register = register;
-        $scope.$on('authenticationSuccess', function() {
+        vm.search = search;
+        vm.searchByClass = searchByClass;
+        vm.loadAll = loadAll;
+        $scope.$on('authenticationSuccess', function () {
             getAccount();
+
         });
+
+        function loadAll() {
+            Note.query(function (result) {
+                vm.notes = result;
+            });
+        }
 
         getAccount();
 
         function getAccount() {
-            Principal.identity().then(function(account) {
+            Principal.identity().then(function (account) {
                 vm.account = account;
                 vm.isAuthenticated = Principal.isAuthenticated;
             });
         }
-        function register () {
+
+        function register() {
             $state.go('register');
         }
+
+        function search() {
+            if (!vm.searchQuery) {
+                return vm.loadAll();
+            }
+            NoteSearch.query({query: vm.searchQuery}, function (result) {
+                console.log(result.toString() + vm.searchQuery.toString());
+                vm.notes = result;
+                $location.url('/note');
+            });
+        }
+
+        function searchByClass() {
+            CourseSearch.query({query: vm.searchQuery2}, function (result) {
+                vm.courses = result;
+                $location.url('/course/1');
+            });
+        }
+
     }
 })();
